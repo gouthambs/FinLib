526f67f586de039609b8808d879189f94c4089ed
Gouthaman Balaraman <gouthambs@gmail.com>
2014-11-15 05:01:14 -0800

    Fix missing file issues

--- finlib/__init__.py
+++ finlib/__init__.py
@@ -1,5 +1,2 @@
 __author__ = 'gbalaraman'
 
-
-from equity.mptstats import MPTStats
-from fi.cashflow import CashflowGenerator, FRMCashflowGenerator
---
+++ finlib/data/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 15 04:47:39 2014
+
+@author: gouthaman
+"""
+
